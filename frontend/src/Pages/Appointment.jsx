c5476e947f4b2cd59c799a6e72e625c99dcb9bd4
parth-cr <parth2004ranpariya@gmail.com>
2024-09-16 15:48:09 +0530

    first commit

---
+++ frontend/src/Pages/Appointment.jsx
@@ -0,0 +1,17 @@
+
+import Hero from "../components/Hero";
+import AppointmentForm from "../components/AppointmentForm";
+
+const Appointment = () => {
+  return (
+    <>
+      <Hero
+        title={"Schedule Your Appointment | ZeeCare Medical Institute"}
+        imageUrl={"/signin.png"}
+      />
+      <AppointmentForm/>
+    </>
+  );
+};
+
+export default Appointment;
